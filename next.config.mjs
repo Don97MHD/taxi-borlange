0eefab8917733920193a322a97b4771bb659e06e
Don97MHD <alimohammad24496@gmail.com>
2026-05-05 15:31:00 +0300

    add slash

--- next.config.mjs
+++ next.config.mjs
@@ -10,7 +10,7 @@ const nextConfig = {
 
   async redirects() {
     return [
-      // تم إزالة /omdomen لكي تعمل الصفحة
+      
       {
         source: '/information',
         destination: '/', 
